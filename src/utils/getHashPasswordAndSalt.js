1b092385252f3ddba91b970e5d13ce5543c61331
whitershade <whitershade34@gmail.com>
2019-11-25 11:37:07 +0200

    added authorization for user

---
+++ src/utils/getHashPasswordAndSalt.js
@@ -0,0 +1,36 @@
+const crypto = require('crypto');
+
+/**
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+const genRandomString = function (length) {
+  return crypto.randomBytes(Math.ceil(length / 2))
+    .toString('hex') /** convert to hexadecimal format */
+    .slice(0, length); /** return required number of characters */
+};
+
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+const getPasswordHashAndSalt = function (password, salt) {
+  const hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+  hash.update(password);
+  const value = hash.digest('hex');
+  return {
+    salt,
+    passwordHash: value,
+  };
+};
+
+function saltHashPassword(userpassword) {
+  const salt = genRandomString(16); /** Gives us salt of length 16 */
+  const passwordData = sha512(userpassword, salt);
+  console.log(`UserPassword = ${userpassword}`);
+  console.log(`Passwordhash = ${passwordData.passwordHash}`);
+  console.log(`nSalt = ${passwordData.salt}`);
+}
